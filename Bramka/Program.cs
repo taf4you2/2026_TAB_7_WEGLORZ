dc9a7ff6e3aa0fc85f5d6cbe451d4d25eead5935
wojtek <wojtek@DESKTOP-MSN5DM6>
2026-04-11 20:30:31 +0200

    utworzono Bramkę and
    taki żart

---
+++ Bramka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bramka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą wartość (0 lub 1):");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą wartość (0 lub 1):");
+            int b = int.Parse(Console.ReadLine());
+            if ((a == 0 || a == 1) && (b == 0 || b == 1))
+            {
+                int wynik = a & b; // Operacja AND
+                Console.WriteLine($"Wynik operacji AND dla {a} i {b} to: {wynik}");
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowe wartości. Proszę podać 0 lub 1.");
+            }
+        }
+    }
+}

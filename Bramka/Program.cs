829645e230f9d69793afb04e896e753fe2432814
wojtek <wojtek@DESKTOP-MSN5DM6>
2026-04-14 10:44:46 +0200

    przeniesiono działanie bramki na API

--- Bramka/Program.cs
+++ Bramka/Program.cs
@@ -1,23 +1,43 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Bramka
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Podaj pierwszą wartość (0 lub 1):");
+            Console.WriteLine("Podaj wartość (0 lub 1):");
             int a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj drugą wartość (0 lub 1):");
+            Console.WriteLine("Podaj wartość (0 lub 1):");
             int b = int.Parse(Console.ReadLine());
+
             if ((a == 0 || a == 1) && (b == 0 || b == 1))
             {
-                int wynik = a & b; // Operacja AND
-                Console.WriteLine($"Wynik operacji AND dla {a} i {b} to: {wynik}");
+                using HttpClient client = new HttpClient();
+
+                string url = $"http://localhost:49226/api/bramka/weryfikuj?a={a}&b={b}";
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    response.EnsureSuccessStatusCode();
+
+                    Console.WriteLine("\nOdpowiedź z serwera API:");
+
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
+                }
+                catch (HttpRequestException e)
+                {
+                    
+                    Console.WriteLine($"\nBłąd połączenia z serwerem API: {e.Message}");
+                }
             }
             else
             {
-                Console.WriteLine("Nieprawidłowe wartości. Proszę podać 0 lub 1.");
+                Console.WriteLine("Nieprawidłowe wartośći");
             }
         }
     }
---
+++ BramkaAPI/Controllers/BramkaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BramkaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BramkaController : ControllerBase
+    {
+        [HttpGet("weryfikuj")]
+        public IActionResult weryfikujDostęp(int a, int b)
+        {
+            if ((a == 0 || a == 1) && (b == 0 || b == 1))
+            {
+                // Oblicza stan logiczny dla dostępu przez bramkę.
+                int wynik = a & b;
+
+                // Zwraca pozytywną odpowiedź wraz z wyliczonym wynikiem.
+                return Ok(new { Wynik = wynik, Wiadomosc = $"Wynik operacji AND dla {a} i {b} to: {wynik}" });
+            }
+            return BadRequest("Podano nie prawidołowe wartości");
+        }
+
+    }
+}
